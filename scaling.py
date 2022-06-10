17275fc0968a326fcf01f6d81b51d865830acb60
Vijay Ram <vijayalayan.ramalingam@target.com>
2022-06-10 15:03:50 -0500

    scaling up even if diff is > 10 -issue#2

--- scaling.py
+++ scaling.py
@@ -3,28 +3,37 @@ from config import CLOUD_URL, CLOUD_APPLICATION, CLOUD_SERVER_GROUP, CLOUD_CLUST
 import time
 
 
+def resize_cluster_batch(scaling_diff, session):
+    for i in range(round(abs(scaling_diff)/4)):
+       resizing_cluster(4, session)
+    print('Update completed')
+
+
 def resize_cluster(new_instance_count):
     print("Updating cluster with new capacity", new_instance_count)
     min_capacity = 2
+    new_instance_count = 4
     if(new_instance_count is not None):
         new_instance_count = round(new_instance_count)
         if(new_instance_count > min_capacity):
             session = fetch_session()
             current_instance_count = fetch_current_cluster(session)
             if(new_instance_count != current_instance_count):
-                    scaling_perct = percentage_change(
-                        new_instance_count, current_instance_count)
-                    print("perct",scaling_perct)
-                    scaling_diff = new_instance_count - current_instance_count
-                    print("scaling_diff",scaling_diff)
-                    if(abs(scaling_perct) > 2.0):  # scale only if diff % more than 2%
-                            # batch it and call method scale cluster iteratively
-                            # to avoud scaling more than 4 at a time
-                            for i in range(round(abs(scaling_diff)/4)):
-                                resizing_cluster(4, session)
-                            print('Update completed')
-                    else:
-                        print("No need to scale")
+                scaling_perct = percentage_change(
+                    new_instance_count, current_instance_count)
+                print("perct", scaling_perct)
+                scaling_diff = new_instance_count - current_instance_count
+                print("scaling_diff", scaling_diff)
+                if(abs(scaling_perct) > 1.0):  # scale only if diff % more than 1%
+                    # batch it and call method scale cluster iteratively
+                    # to avoud scaling more than 4 at a time
+                    resize_cluster_batch(scaling_diff, session)
+                # only do scale up if diff is less than 1%
+                elif(abs(scaling_perct) <= 1.0 and scaling_diff < 0):
+                    print("only scaling up") # no need to scale down if less than 1%
+                    resize_cluster_batch(scaling_diff, session)
+                else:
+                    print("No need to scale")
             else:
                 print("No need to scale")
 
@@ -41,14 +50,16 @@ def fetch_session():
                                   json={'username': CLOUD_USER,
                                         'password': CLOUD_PASSWORD},
                                   verify=False)
-    print("token",token_response)
+    # print("token",token_response)
     return session
 
-##TODO: fetch list of all server groups to get the active one. remove hard coded value
+# TODO: fetch list of all server groups to get the active one. remove hard coded value
+
+
 def fetch_current_cluster(session):
     cluster_info = session.get(url=CLOUD_URL+'/api/applications/'+CLOUD_APPLICATION+'/clusters/'+CLOUD_CLUSTER+'/dev/server_groups/'+CLOUD_SERVER_GROUP,
                                verify=False)
-    print(cluster_info.json())
+    # print(cluster_info.json())
     return cluster_info.json()['instanceCounts']['total']
 
 
@@ -70,8 +81,8 @@ def resizing_cluster(new_instance_count, session):
 
 
 def percentage_change(new_instance_count, current_instance_count):
-     divider = min(new_instance_count, current_instance_count)
-     return (new_instance_count - current_instance_count)/divider
+    divider = min(new_instance_count, current_instance_count)
+    return (new_instance_count - current_instance_count)/divider
 
 
 if __name__ == '__main__':
