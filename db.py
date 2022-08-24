a9153fe9d93c8008c89aeabdc55cff9e15254014
Vijay Ram <vijayalayan.ramalingam@target.com>
2022-08-24 15:26:54 -0500

    added support for manual override

--- db.py
+++ db.py
@@ -13,11 +13,28 @@ def get_forecast_next_run():
     engine = create_engine(postgres_str)
     current_ts =  str(datetime.utcnow())
     print(current_ts)
-    sql = "SELECT yhat FROM oph_forecast where ds>{} ORDER BY ds DESC LIMIT 1;".format("'"+current_ts+"'")
+    sql = "SELECT yhat,manual_override,override_order_per_hr FROM oph_forecast where ds>{} ORDER BY ds DESC LIMIT 1;".format("'"+current_ts+"'")
+    #sql = "select yhat,manual_override,override_order_per_hr  where ds>{}from oph_forecast order by ds DESC  limit 1"
     result = engine.execute(sql)
-    #print(result.fetchone()[0]) // always comment this out when running the script
-    return result.fetchone()[0]
+    #print(result.fetchone()[0]) # always comment this out when running the script
+    return result.fetchone()
 
+
+def get_forecast_last_run():
+    """ query near future estimated order count from the oph_forecast table. 
+    this returns only (one row) the last known count """
+
+    postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'
+    .format(username=POSTGRES_USERNAME,password=POSTGRES_PASSWORD,ipaddress=POSTGRES_ADDRESS,port=POSTGRES_PORT,dbname=POSTGRES_DBNAME))
+
+    engine = create_engine(postgres_str)
+    current_ts =  str(datetime.utcnow())
+    print(current_ts)
+    sql = "SELECT yhat,manual_override,override_order_per_hr FROM oph_forecast ORDER BY ds DESC LIMIT 1;".format("'"+current_ts+"'")
+    #sql = "select yhat,manual_override,override_order_per_hr  where ds>{}from oph_forecast order by ds DESC  limit 1"
+    result = engine.execute(sql)
+    #print(result.fetchone()[0]) # always comment this out when running the script
+    return result.fetchone()
 
 if __name__ == '__main__':
     get_forecast_next_run()
