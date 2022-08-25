dc87497d36c58a8ad0aa325d7611d6591a5c206f
Vijay Ram <vijayalayan.ramalingam@target.com>
2022-08-25 16:15:11 -0500

    fixing date so that it takes the today's date from db

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import close_all_sessions
-from datetime import datetime
+from datetime import date
 from config import POSTGRES_ADDRESS, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DBNAME ,POSTGRES_PORT
+from dateutil.relativedelta import relativedelta
 
 def get_forecast_next_run():
     """ query near future estimated order count from the oph_forecast table. 
@@ -11,12 +12,13 @@ def get_forecast_next_run():
     .format(username=POSTGRES_USERNAME,password=POSTGRES_PASSWORD,ipaddress=POSTGRES_ADDRESS,port=POSTGRES_PORT,dbname=POSTGRES_DBNAME))
 
     engine = create_engine(postgres_str)
-    current_ts =  str(datetime.utcnow())
-    print(current_ts)
-    sql = "SELECT yhat,manual_override,override_order_per_hr FROM oph_forecast where ds>{} ORDER BY ds DESC LIMIT 1;".format("'"+current_ts+"'")
+    current_dt = date.today()
+    next_dt = date.today() + relativedelta(days=+1)
+    print(next_dt)
+    sql = "SELECT yhat,manual_override,override_order_per_hr FROM oph_forecast where ds>{} and ds<{} ORDER BY ds DESC LIMIT 1;".format("'"+str(current_dt)+"'", "'"+str(next_dt)+"'")
     #sql = "select yhat,manual_override,override_order_per_hr  where ds>{}from oph_forecast order by ds DESC  limit 1"
     result = engine.execute(sql)
-    #print(result.fetchone()[0]) # always comment this out when running the script
+    #print(result.fetchone()) # always comment this out when running the script
     return result.fetchone()
 
 
@@ -28,12 +30,12 @@ def get_forecast_last_run():
     .format(username=POSTGRES_USERNAME,password=POSTGRES_PASSWORD,ipaddress=POSTGRES_ADDRESS,port=POSTGRES_PORT,dbname=POSTGRES_DBNAME))
 
     engine = create_engine(postgres_str)
-    current_ts =  str(datetime.utcnow())
-    print(current_ts)
-    sql = "SELECT yhat,manual_override,override_order_per_hr FROM oph_forecast ORDER BY ds DESC LIMIT 1;".format("'"+current_ts+"'")
+    current_dt =  str(date.today())
+    print(current_dt)
+    sql = "SELECT yhat FROM oph_forecast where manual_override = false ORDER BY ds DESC LIMIT 1;".format("'"+current_dt+"'")
     #sql = "select yhat,manual_override,override_order_per_hr  where ds>{}from oph_forecast order by ds DESC  limit 1"
     result = engine.execute(sql)
-    #print(result.fetchone()[0]) # always comment this out when running the script
+    #print(result.fetchone()) # always comment this out when running the script
     return result.fetchone()
 
 if __name__ == '__main__':
