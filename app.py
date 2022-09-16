543188a35e7732015ed8844860146f1b0d697101
Vijay Ram <vijayalayan.ramalingam@target.com>
2022-09-16 07:26:52 -0500

    refactoring job schedule

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from logging.config import fileConfig
 from flask import Flask, jsonify
 from targetoss_tappy import Configuration
-from jobscheduler import job
 import targetoss_tappy as tappy
+from flask_apscheduler import APScheduler
 
-# fileConfig('logging.conf')
 app = Flask(__name__)
 
+scheduler = APScheduler()
 
 @app.route('/health')
 def health():
@@ -26,7 +26,31 @@ def config():
     return jsonify(conf.data.get("cart", "config_value isn't in Consul!"))
 
 
+def scheduleScalingTask(configuration_data):
+    resize_server_capacity(configuration_data)
+
+
+
+@scheduler.task('cron', id='do_job_1', hour=0, minute='1')
+def job(configuration_data=dict()):
+    conf = Configuration()
+    if(conf.data):
+        job_details = conf.data["job"]
+        job_hr = job_details["hour"]
+        job_minute = job_details["minute"]
+    else:
+        job_hr = 1
+        job_minute = 20
+
+    app.logger.info("Job scheduler details {} {} ".format(job_hr, job_minute))
+    # add more jobs bot_scanner job later
+    scheduleScalingTask(configuration_data)
+
 if __name__ == '__main__':
     conf = Configuration()
-    job(conf.data)
+   # app.config.from_object(Config())
+    scheduler.init_app(app)
+    scheduler.start()
+
+    app.run()
     app.run(debug=False, use_reloader=False, host='0.0.0.0', port=8080)
