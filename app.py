ff7be1b6e7bb99896c7133a05e5f70584ee1062c
Vijay Ram <vijayalayan.ramalingam@target.com>
2022-09-19 09:52:32 -0500

    added cron job from properites

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from targetoss_tappy import Configuration
 import targetoss_tappy as tappy
 from flask_apscheduler import APScheduler
 from controller import resize_server_capacity
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def health():
 @app.route('/')
 def index():
     app.logger.info("hello - this is logging!")
-    return jsonify({'status': 'up'})
+    return jsonify({'time': datetime.now()})
 
 
 @app.route('/config')
@@ -27,22 +28,30 @@ def config():
     return jsonify(conf.data.get("cart", "config_value isn't in Consul!"))
 
 
-def scheduleScalingTask(configuration_data):
-    resize_server_capacity(configuration_data)
+def scheduleScalingTask(config):
+    resize_server_capacity(config)
 
-@scheduler.task('cron', id='do_job_1', hour=8, minute='30')
-def job(configuration_data=dict()):
+#@scheduler.task('cron', id='do_job_1', hour=8, minute='30')
+def job():
     if tappy.in_tap():
         config = get_tap_config()
+        job_details = config["job"]
+        job_hr = db_details["hour"]
+        job_min = db_details["minute"]
     else:
         config = Configuration()
+        job_hr = 9
+        job_min = 52
 
-    app.logger.info("Job scheduler details {} ".format(config.data))
-    # add more jobs bot_scanner job later
-    scheduleScalingTask(config)
+    scheduler.add_job(id='Scheduled Task1', func=scheduleScalingTask(config),
+                      trigger="cron", hour=job_hr, minute=job_min)
+    scheduler.start()
+    app.logger.info("Job scheduler details ")
+
+
 
 if __name__ == '__main__':
    # app.config.from_object(Config())
-    scheduler.init_app(app)
-    scheduler.start()
+    scheduler.init_app(app)    
+    job()
     app.run(debug=False, use_reloader=False, host='0.0.0.0', port=8080)
